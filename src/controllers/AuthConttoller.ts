f27a995b4a93a35438679dd54f019e1d810f2d88
davidOiveira <143909652+davidOiveira@users.noreply.github.com>
2025-12-03 22:52:24 -0300

    Rota autenticação de login

--- src/controllers/AuthConttoller.ts
+++ src/controllers/AuthConttoller.ts
@@ -1,9 +1,32 @@
-import express, {Request, Response} from "express";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Importar a biblioteca Express
+const express_1 = __importDefault(require("express"));
+const AuthService_1 = require("../services/AuthService");
+// Criar a Aplicação Express
+const router = express_1.default.Router();
 
-const router = express.Router();
-
-router.get("/", (req:Request, res:Response) =>{
-    res.send("TESTE")
-})
-
-export default router
+router.post("/", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            res.status(400).json({
+                message: "Email e senha são obrigatórios!",
+            });
+            return;
+        }
+        const authService = new AuthService_1.AuthService();
+        const userData = await authService.login(email, password);
+        // Retornar erro em caso de falha
+        res.status(200).json({
+            message: "Login Realizado com Sucesso!",
+            user: userData
+        });
+        return;
+    }
+    catch (error) {
+        // Retornar erro em caso de falha
+        res.status(401).json({
+            message: error.message || "Erro ao realizar o login!",
+        });
+        return;
+    }
