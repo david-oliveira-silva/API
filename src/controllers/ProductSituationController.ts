e4754bfc97af7a694cff323ecb905d2abb762043
davidOiveira <143909652+davidOiveira@users.noreply.github.com>
2025-12-03 22:44:07 -0300

    Rota teste Front-end

--- src/controllers/ProductSituationController.ts
+++ src/controllers/ProductSituationController.ts
@@ -1,149 +1,149 @@
-import { error } from "console";
-import express, {Request, Response} from "express";
-import { AppDataSource } from "../data-source";
-import { Situations } from "../entity/Situations";
-import { checkPrimeSync } from "crypto";
-import { PaginationService } from "../services/PaginationService";
-import { ProductSituation } from "../entity/ProductSituation";
+    import { error } from "console";
+    import express, {Request, Response} from "express";
+    import { AppDataSource } from "../data-source";
+    import { Situations } from "../entity/Situations";
+    import { checkPrimeSync } from "crypto";
+    import { PaginationService } from "../services/PaginationService";
+    import { ProductSituation } from "../entity/ProductSituation";
 
-const router = express.Router();
+    const router = express.Router();
 
-router.get("/productSituation", async (req:Request, res:Response) =>{
-    console.log("➡️ Entrou na rota /productCategoria");
-    try{ 
-        const situationRepository = AppDataSource.getRepository(ProductSituation);
-        console.log("📦 Repository carregado:", situationRepository.metadata.tableName);
+    router.get("/productSituation", async (req:Request, res:Response) =>{
+        console.log("➡️ Entrou na rota /productCategoria");
+        try{ 
+            const situationRepository = AppDataSource.getRepository(ProductSituation);
+            console.log("📦 Repository carregado:", situationRepository.metadata.tableName);
 
-        const page = Number(req.query.page) || 1;
-        const limite = Number(req.query.limite) || 10;
+            const page = Number(req.query.page) || 1;
+            const limite = Number(req.query.limite) || 10;
 
-        console.log(`📄 page=${page}, limite=${limite}`);
+            console.log(`📄 page=${page}, limite=${limite}`);
 
-        const result = await PaginationService.paginate(situationRepository, page, limite, {id: "DESC"});
-        console.log("✅ Paginate retornou", result);
+            const result = await PaginationService.paginate(situationRepository, page, limite, {id: "DESC"});
+            console.log("✅ Paginate retornou", result);
 
-        res.status(200).json(result);
-        return;
+            res.status(200).json(result);
+            return;
+            
+        } catch(error){
+            console.error("❌ Erro na rota /productCategoria:", error);
+            res.status(500).json({
+                mensagem: "Erro ao listar situações!"
+            });
+            return;
+        }
+    });
+
+    router.get("/productSituation/:id", async (req:Request, res:Response) =>{
+        try{
+
+            const {id} = req.params;
+
+            const situationRepository = AppDataSource.getRepository(ProductSituation);
+
+            const situation = await situationRepository.findOneBy({id : parseInt(id)});
+
+            if(!situation){
+                res.status(404).json({
+                mensagem: "Situação não encontrada!"
+                });
+                return
+            }
+
+            res.status(200).json(situation);
+            return
+
+        } catch(error){
+            res.status(500).json({
+                mensagem: "Erro ao visualizar situação!"
+            });
+            return
+        }
+    })
+
+    router.post("/productSituation", async(req:Request, res:Response) =>{
         
-    } catch(error){
-        console.error("❌ Erro na rota /productCategoria:", error);
-        res.status(500).json({
-            mensagem: "Erro ao listar situações!"
-        });
-        return;
-    }
-});
+        try{
+            var data = req.body;
 
-router.get("/productSituation/:id", async (req:Request, res:Response) =>{
-    try{
+            const situationRepository = AppDataSource.getRepository(ProductSituation);
+            const newSituation = situationRepository.create(data);
 
-        const {id} = req.params;
+            await situationRepository.save(newSituation);
 
-        const situationRepository = AppDataSource.getRepository(ProductSituation);
+            res.status(201).json({
+                mensagem : "Situação cadastrada com sucesso!",
+                situation: newSituation,
+            });
+        } catch(error){
+            console.error("❌ Erro ao cadastrar situação:", error);
+            res.status(500).json({
+                mensagem : "Erro ao cadastrar situação!",
+            });
+        }
+    })
 
-        const situation = await situationRepository.findOneBy({id : parseInt(id)});
+    router.put("/productSituation/:id", async (req:Request, res:Response) =>{
+        try{
 
-        if(!situation){
-            res.status(404).json({
-            mensagem: "Situação não encontrada!"
-             });
+            const {id} = req.params;
+
+            var data = req.body;
+
+            const situationRepository = AppDataSource.getRepository(ProductSituation);
+
+            const categoria = await situationRepository.findOneBy({id : parseInt(id)});
+
+            if(!categoria){
+                res.status(404).json({
+                mensagem: "Situação não encontrada!"
+                });
+                return
+            }
+
+            situationRepository.merge(categoria, data);
+
+            const updateCategoria = await situationRepository.save(categoria);
+
+            res.status(200).json({
+                mensagem: "Situação atualizada com sucesso!",
+                situations: updateCategoria,
+            });
+        } catch(error){
+            res.status(500).json({
+                mensagem: "Erro ao atualizar situação!"
+            });
             return
         }
+    })
 
-        res.status(200).json(situation);
-        return
+    router.delete("/productSituation/:id", async (req:Request, res:Response) =>{
+        try{
 
-    } catch(error){
-        res.status(500).json({
-            mensagem: "Erro ao visualizar situação!"
-        });
-        return
-    }
-})
+            const {id} = req.params;
 
-router.post("/productSituation", async(req:Request, res:Response) =>{
-    
-    try{
-        var data = req.body;
+            const situationRepository = AppDataSource.getRepository(ProductSituation);
 
-        const situationRepository = AppDataSource.getRepository(ProductSituation);
-        const newSituation = situationRepository.create(data);
+            const categoria = await situationRepository.findOneBy({id : parseInt(id)});
 
-        await situationRepository.save(newSituation);
+            if(!categoria){
+                res.status(404).json({
+                mensagem: "Situação não encontrada!"
+                });
+                return
+            }
 
-        res.status(201).json({
-            mensagem : "Situação cadastrada com sucesso!",
-            situation: newSituation,
-        });
-    } catch(error){
-        console.error("❌ Erro ao cadastrar situação:", error);
-        res.status(500).json({
-            mensagem : "Erro ao cadastrar situação!",
-        });
-    }
-})
+            await situationRepository.remove(categoria);
 
-router.put("/productSituation/:id", async (req:Request, res:Response) =>{
-    try{
-
-        const {id} = req.params;
-
-        var data = req.body;
-
-        const situationRepository = AppDataSource.getRepository(ProductSituation);
-
-        const categoria = await situationRepository.findOneBy({id : parseInt(id)});
-
-        if(!categoria){
-            res.status(404).json({
-            mensagem: "Situação não encontrada!"
-             });
+            res.status(200).json({
+                mensagem: "Situação removida com sucesso!",
+            });
+        } catch(error){
+            res.status(500).json({
+                mensagem: "Erro ao remover situação!"
+            });
             return
         }
+    })
 
-        situationRepository.merge(categoria, data);
-
-        const updateCategoria = await situationRepository.save(categoria);
-
-        res.status(200).json({
-            mensagem: "Situação atualizada com sucesso!",
-            situations: updateCategoria,
-        });
-    } catch(error){
-        res.status(500).json({
-            mensagem: "Erro ao atualizar situação!"
-        });
-        return
-    }
-})
-
-router.delete("/productSituation/:id", async (req:Request, res:Response) =>{
-    try{
-
-        const {id} = req.params;
-
-        const situationRepository = AppDataSource.getRepository(ProductSituation);
-
-        const categoria = await situationRepository.findOneBy({id : parseInt(id)});
-
-        if(!categoria){
-            res.status(404).json({
-            mensagem: "Situação não encontrada!"
-             });
-            return
-        }
-
-        await situationRepository.remove(categoria);
-
-        res.status(200).json({
-            mensagem: "Situação removida com sucesso!",
-        });
-    } catch(error){
-        res.status(500).json({
-            mensagem: "Erro ao remover situação!"
-        });
-        return
-    }
-})
-
-export default router
+    export default router
